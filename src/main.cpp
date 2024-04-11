12746037d63e13b8d5893d28ef9dfca7b3515854
johnnythor00 <irainocencio00@gmail.com>
2024-04-12 01:54:34 +0800

    Lab 3 activity

---
+++ src/main.cpp
@@ -0,0 +1,52 @@
+/* ****************************************************************
+ * @author: <John Ira Inocencio>
+ * @app name: <firstCode>
+ * @app desc: <My first C++ code>
+ * @history:
+ *  - <yyyy/mm/dd> | <name>
+ *      -- <description>
+ *      -- <description>
+ *      -- <description>
+ *  - <yyyy/mm/dd> | <name>
+ *      -- <description>
+ *      -- <description>
+ *      -- <description>
+ * ****************************************************************/
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include "_pause.h"
+
+using namespace std;
+
+//////////////////////////////////////////////////////////////////
+//                               NOTE
+// This is your program entry point. Your main logic is placed
+// inside this function. You may add your functions before this
+// "main()", or after this "main()" provided you added reference
+// before this "main()" function.
+//////////////////////////////////////////////////////////////////
+
+int main() {
+
+      std::cout << "H";
+    std::cout << "e";
+    std::cout << "l";
+    std::cout << "l";
+    std::cout << "o";
+    std::cout << std::endl;
+    
+    // Display "World" in another line
+    std::cout << "W";
+    std::cout << "o";
+    std::cout << "r";
+    std::cout << "l";
+    std::cout << "d";
+    std::cout << std::endl;
+    
+    return 0;
+   
+
+     
+}
