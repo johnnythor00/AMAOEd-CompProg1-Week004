d8ebac3ce4a0ba4454208f4cfa61753e4a18b1af
johnnythor00 <irainocencio00@gmail.com>
2024-04-12 18:21:55 +0800

    machine problem 2

--- src/main.cpp
+++ src/main.cpp
@@ -26,40 +26,113 @@ using namespace std;
 // inside this function. You may add your functions before this
 // "main()", or after this "main()" provided you added reference
 // before this "main()" function.
-//////////////////////////////////////////////////////////////////
+/////////
+
+//7 columns 5 rows
+
+
+const int ROWS= 5;
+const int COLUMNS= 7;
+
+
+
+//functions
+
+void displaySeats(int seats[][COLUMNS]) {
+    for (int i = 0; i < ROWS; ++i) {
+        for (int j = 0; j < COLUMNS; ++j) {
+            cout << seats[i][j] << "\t";
+        }
+        cout << endl;
+    }
+}
+
+
+bool isValidSeat(int seatNum) {
+
+    return seatNum >= 1 && seatNum <= ROWS * COLUMNS;
+}
+
+void reserveSeat(int seats[][COLUMNS], int seatNum) {
+
+    int row = (seatNum - 1) / COLUMNS;
+    int col = (seatNum - 1) % COLUMNS;
+
+
+
+    if (seats[row][col] == 0)
+    {
+        cout << "Seat is already taken" << endl;
+
+    } else {
+        seats[row][col] = 0;
+        cout << "Seat succesfully reserved." << endl;
+
+    }
+
+}
+
 
 int main() {
-    // Define an array to store the integers
-    int integers[6];
 
-    // Prompt the user to enter six integers
-    cout << "Enter six integers, separated by spaces: ";
+int seats[ROWS][COLUMNS];
+int seatNum;
 
-    // Read the integers into the array
-    for (int i = 0; i < 6; i++) {
-        cin >> integers[i];
+int count = 1;
+
+for (int i = 0; i < ROWS; i++)
+{
+    for (int j = 0; j < COLUMNS; j++)
+    {
+        seats[i][j] = count++;
     }
-
-    // Bubble sort algorithm to sort the integers in ascending order
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5 - i; j++) {
-            if (integers[j] > integers[j + 1]) {
-                // Swap the integers if they are out of order
-                int temp = integers[j];
-                integers[j] = integers[j + 1];
-                integers[j + 1] = temp;
-            }
-        }
-    }
-
-    // Display the sorted integers
-    cout << "The sorted integers are: ";
-    for (int i = 0; i < 6; i++) {
-        cout << integers[i] << " ";
-    }
-    cout << endl;
-
-    return 0;
 }
+
+displaySeats(seats);
+
+
+
+//reserve
+
+
+while (true)
+{
+    cout << "Enter seat number to reserve:"; 
+    cin >> seatNum;
+
+
+if (!isValidSeat(seatNum))
+{
+    cout << "Invalid seat number. Please enter a valid seat number." << endl;
+    continue;
+}
+
+
+reserveSeat(seats, seatNum);
+displaySeats(seats);
+
+    }
+
+return 0;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
